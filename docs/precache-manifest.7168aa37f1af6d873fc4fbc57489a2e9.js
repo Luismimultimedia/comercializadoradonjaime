f9e0772f1a146ba1ac2a3f66bd5a153413f385c9
Luis Miguel Gonzalez <luis.gonzalez@olsoftware.com>
2020-09-28 15:36:02 -0500

    fixes

--- docs/precache-manifest.7168aa37f1af6d873fc4fbc57489a2e9.js
+++ docs/precache-manifest.7168aa37f1af6d873fc4fbc57489a2e9.js
@@ -5,70 +5,70 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "6440ef4a15e9470ebcf3",
-    "url": "/static/css/2.8aa5a7f8.chunk.css"
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
   },
   {
     "revision": "941411caaebf08aa6ea2",
-    "url": "/static/css/main.bd9acaff.chunk.css"
+    "url": "./static/css/main.bd9acaff.chunk.css"
   },
   {
     "revision": "6440ef4a15e9470ebcf3",
-    "url": "/static/js/2.1ef19422.chunk.js"
+    "url": "./static/js/2.1ef19422.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.1ef19422.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.1ef19422.chunk.js.LICENSE.txt"
   },
   {
     "revision": "941411caaebf08aa6ea2",
-    "url": "/static/js/main.d8f533c5.chunk.js"
+    "url": "./static/js/main.d8f533c5.chunk.js"
   },
   {
     "revision": "9152b2d1b7f36c75cd41",
-    "url": "/static/js/runtime-main.790addb9.js"
+    "url": "./static/js/runtime-main.790addb9.js"
   },
   {
     "revision": "e3370fbd5d36c33a43b916aa4db170ab",
-    "url": "/static/media/ala.e3370fbd.png"
+    "url": "./static/media/ala.e3370fbd.png"
   },
   {
     "revision": "637b52db058fa2b576727013265ba00a",
-    "url": "/static/media/contramuslo.637b52db.png"
+    "url": "./static/media/contramuslo.637b52db.png"
   },
   {
     "revision": "3346a15a36b0ccb2ac4505abee085125",
-    "url": "/static/media/imgIntroduction.3346a15a.jpg"
+    "url": "./static/media/imgIntroduction.3346a15a.jpg"
   },
   {
     "revision": "ce1d3e4d30a286300ec8f7a7f3d55d1d",
-    "url": "/static/media/map.ce1d3e4d.png"
+    "url": "./static/media/map.ce1d3e4d.png"
   },
   {
     "revision": "7fb6096f7c009e6b4a20f8bf8fb47f13",
-    "url": "/static/media/muslo.7fb6096f.png"
+    "url": "./static/media/muslo.7fb6096f.png"
   },
   {
     "revision": "a77abe17bc500cbfbcaab429c4b5be29",
-    "url": "/static/media/pechuga.a77abe17.png"
+    "url": "./static/media/pechuga.a77abe17.png"
   },
   {
     "revision": "7e5e6857ecc197f4d00820da62a3b826",
-    "url": "/static/media/photo1.7e5e6857.png"
+    "url": "./static/media/photo1.7e5e6857.png"
   },
   {
     "revision": "996fd2d237a0b7b1131d923beff8d8b5",
-    "url": "/static/media/photo2.996fd2d2.png"
+    "url": "./static/media/photo2.996fd2d2.png"
   },
   {
     "revision": "be88fe78ea31ced021e3bea64188d150",
-    "url": "/static/media/pollo-naranja.be88fe78.png"
+    "url": "./static/media/pollo-naranja.be88fe78.png"
   },
   {
     "revision": "bd97c02fd15a6a11b85d3a8717870ae1",
-    "url": "/static/media/pollo-sudado.bd97c02f.png"
+    "url": "./static/media/pollo-sudado.bd97c02f.png"
   },
   {
     "revision": "95a38487c4ffa9e50d6a6e0b5c1a0f6a",
-    "url": "/static/media/pollo.95a38487.png"
+    "url": "./static/media/pollo.95a38487.png"
   }
 ]);
