6d2743ba0f58d18dcc990bea02dd4f19938a6733
Luis Miguel Gonzalez <luis.gonzalez@olsoftware.com>
2020-09-28 17:58:23 -0500

    add in to the package.json the url

---
+++ docs/precache-manifest.b6184ac77a9f8b156aa209fbe2b59f2d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53121f22c4ab1598fba8374d2d06779",
+    "url": "/comercializadoradonjaime/index.html"
+  },
+  {
+    "revision": "6440ef4a15e9470ebcf3",
+    "url": "/comercializadoradonjaime/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "9392aec8b565db7169fc",
+    "url": "/comercializadoradonjaime/static/css/main.bd9acaff.chunk.css"
+  },
+  {
+    "revision": "6440ef4a15e9470ebcf3",
+    "url": "/comercializadoradonjaime/static/js/2.1ef19422.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/comercializadoradonjaime/static/js/2.1ef19422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9392aec8b565db7169fc",
+    "url": "/comercializadoradonjaime/static/js/main.86f09861.chunk.js"
+  },
+  {
+    "revision": "897851299591fc49df0c",
+    "url": "/comercializadoradonjaime/static/js/runtime-main.6f16aa88.js"
+  },
+  {
+    "revision": "e3370fbd5d36c33a43b916aa4db170ab",
+    "url": "/comercializadoradonjaime/static/media/ala.e3370fbd.png"
+  },
+  {
+    "revision": "637b52db058fa2b576727013265ba00a",
+    "url": "/comercializadoradonjaime/static/media/contramuslo.637b52db.png"
+  },
+  {
+    "revision": "3346a15a36b0ccb2ac4505abee085125",
+    "url": "/comercializadoradonjaime/static/media/imgIntroduction.3346a15a.jpg"
+  },
+  {
+    "revision": "ce1d3e4d30a286300ec8f7a7f3d55d1d",
+    "url": "/comercializadoradonjaime/static/media/map.ce1d3e4d.png"
+  },
+  {
+    "revision": "7fb6096f7c009e6b4a20f8bf8fb47f13",
+    "url": "/comercializadoradonjaime/static/media/muslo.7fb6096f.png"
+  },
+  {
+    "revision": "a77abe17bc500cbfbcaab429c4b5be29",
+    "url": "/comercializadoradonjaime/static/media/pechuga.a77abe17.png"
+  },
+  {
+    "revision": "7e5e6857ecc197f4d00820da62a3b826",
+    "url": "/comercializadoradonjaime/static/media/photo1.7e5e6857.png"
+  },
+  {
+    "revision": "996fd2d237a0b7b1131d923beff8d8b5",
+    "url": "/comercializadoradonjaime/static/media/photo2.996fd2d2.png"
+  },
+  {
+    "revision": "be88fe78ea31ced021e3bea64188d150",
+    "url": "/comercializadoradonjaime/static/media/pollo-naranja.be88fe78.png"
+  },
+  {
+    "revision": "bd97c02fd15a6a11b85d3a8717870ae1",
+    "url": "/comercializadoradonjaime/static/media/pollo-sudado.bd97c02f.png"
+  },
+  {
+    "revision": "95a38487c4ffa9e50d6a6e0b5c1a0f6a",
+    "url": "/comercializadoradonjaime/static/media/pollo.95a38487.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39749deb064d99547b4f62ab82bb484a.js"
+  "/comercializadoradonjaime/precache-manifest.b6184ac77a9f8b156aa209fbe2b59f2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/comercializadoradonjaime/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
